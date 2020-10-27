a355158d2d5436f2e828ac7bb214b6d669c25b97
Daniil Nichitenco <daniilnikitenco@gmail.com>
2020-10-27 13:19:42 +0200

    Added OccationProfile

---
+++ src/ExpenseTracker.API/Map profiles/OccasionProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ExpenseTracker.API.Dtos.Occasions;
+using ExpenseTracker.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.API.Map_profiles
+{
+    public class OccasionProfile : Profile
+    {
+        public OccasionProfile()
+        {
+            CreateMap<Occasion, OccasionDto>();
+            CreateMap<OccasionForUpdateDto, Occasion>();
+        }
+    }
+}
